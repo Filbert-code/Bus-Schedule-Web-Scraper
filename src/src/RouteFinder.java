d0bf54d3c8180c5d55e2b51405eacdd564555c09
Filbert-code <alexlingfungfilbert@gmail.com>
2021-01-28 21:47:14 -0800

    Added implementation for getUrlText function

--- src/src/RouteFinder.java
+++ src/src/RouteFinder.java
@@ -1,3 +1,7 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.URL;
+import java.net.URLConnection;
 import java.util.HashMap;
 import java.util.LinkedHashMap;
 import java.util.Map;
@@ -6,13 +10,24 @@ public class RouteFinder implements IRouteFinder{
 
     private String text;
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception{
         RouteFinder rf = new RouteFinder();
-//        rf.getUrlText();
+        String URL = "https://www.communitytransit.org/busservice/schedules/";
+        String html = rf.getUrlText(URL);
+        System.out.println(html);
     }
 
-    private String getUrlText(String URL) {
-        return "";
+    private String getUrlText(String URL) throws Exception{
+        URLConnection bus_sch_website = new URL(URL).openConnection();
+        bus_sch_website.setRequestProperty("user-Agent", "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.11 (KHTML, like Gecko) Chrome/23.0.1271.95 Safari/537.11");
+        BufferedReader in = new BufferedReader(new InputStreamReader(bus_sch_website.getInputStream()));
+        String inputLine = "";
+        String text = "";
+        while ((inputLine = in.readLine()) != null) {
+            this.text += inputLine + "\n";
+        }
+        in.close();
+        return this.text;
     }
 
     public Map<String, Map<String, String>> getBusRoutesUrls(final char destInitial) {
