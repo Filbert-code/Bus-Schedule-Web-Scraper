64c1bdc90063bd17131cacecd5efe501aa6b2e36
Filbert-code <alexlingfungfilbert@gmail.com>
2021-02-05 03:53:51 -0800

    Added additional error handling for getRouteUrls and getRouteStops

--- src/src/Client.java
+++ src/src/Client.java
@@ -7,44 +7,40 @@ import java.util.Scanner;
 
 public class Client {
     public static void main(String[] args) throws Exception{
+        
+        RouteFinder rf = new RouteFinder();
+        Scanner scanner = new Scanner(System.in);
+        boolean running = true;
+        while(running) {
+            System.out.print("Please enter a letter that your destination starts with: ");
+            char dest = scanner.next().charAt(0);
+            System.out.println();
+            Map<String,Map<String, String>> map = printBusRoutesUrls(rf, dest);
 
-        RouteFinder r = new RouteFinder();
-        Map<String, LinkedHashMap<String, String>> map2 = r.getRouteStops("This is not a URL");
+            System.out.print("Please enter your destination: ");
+            String dest_complete = scanner.next();
+            System.out.print("Please enter your route ID: ");
+            String route_id = scanner.next();
+            System.out.println();
 
+            String route_url = "";
+            try {
+                route_url = map.get(dest_complete).get(route_id);
+            } catch(Exception e) {
+                System.out.println("Could not find a bus schedule with the given destination and route combination.\n");
+                continue;
+            }
 
-//        RouteFinder rf = new RouteFinder();
-//        Scanner scanner = new Scanner(System.in);
-//        boolean running = true;
-//        while(running) {
-//            System.out.print("Please enter a letter that your destination starts with: ");
-//            char dest = scanner.next().charAt(0);
-//            System.out.println();
-//            Map<String,Map<String, String>> map = printBusRoutesUrls(rf, dest);
-//
-//            System.out.print("Please enter your destination: ");
-//            String dest_complete = scanner.next();
-//            System.out.print("Please enter your route ID: ");
-//            String route_id = scanner.next();
-//            System.out.println();
-//
-//            String route_url = "";
-//            try {
-//                route_url = map.get(dest_complete).get(route_id);
-//            } catch(Exception e) {
-//                System.out.println("Could not find a bus schedule with the given destination and route combination.\n");
-//                continue;
-//            }
-//
-//            printRouteStops(rf, route_url);
-//
-//            System.out.println();
-//            System.out.print("Do you want to check a different destination? Please type Y to continue or press any other key to exit ");
-//            String user_input = scanner.next();
-//            if(!user_input.toLowerCase().equals("y")) {
-//                running = false;
-//            }
-//            System.out.println();
-//        }
+            printRouteStops(rf, route_url);
+
+            System.out.println();
+            System.out.print("Do you want to check a different destination? Please type Y to continue or press any other key to exit ");
+            String user_input = scanner.next();
+            if(!user_input.toLowerCase().equals("y")) {
+                running = false;
+            }
+            System.out.println();
+        }
 
     }
 
